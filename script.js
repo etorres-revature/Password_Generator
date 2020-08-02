b3d0b56d4526ed2a09f33c95ea9988f12c9a85ce
Eric D. Torres <etorresnotary@gmail.com>
2020-08-01 20:16:59 -0500

    adding logic for writepassword function

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //number array
-let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //special character array
-let special =["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"]
+let special =["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"];
+
+//array to hold choices from prompts
+let choice = [];
 
 //function to choose random lower case letter from lower array
 function getRandomLower() {
@@ -38,11 +41,22 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   let passwordLength = prompt("Please choose a length for you password \nPasswords must be a minimum of 8 characters and a maximum of 128 characters.");
-  let hasLower = confirm("Do you want your passwrod to contain lower case characters?\n OK = YES and Cancel = NO");
+    if (!enter) {
+      alert("Please enter a value")
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      alert("Please input a valid password length\nminimum number of characters = 8\nmaximum number of characters = 128")
+    } else {
+  let hasLower = confirm("Do you want your password to contain lower case characters?\n OK = YES and Cancel = NO");
   let hasUpper = confirm("Do you want your password to contain upper case characters?\n OK = YES and Cancel = NO");
   let hasNumber = confirm("Do you want your password to contain numbers\n OK = YES and Cancel = NO");
   let hasSpecial = confirm("Do you want your password to contain special characters\n OK = YES and Cancel =NO")
+    }
+    if (!hasLower && !hasUpper && !hasNumber && !hasSpecial) {
+      choices = alert("Please choose at least one criteria")
+    }
   
+function generatePassword();
+
   passwordText.value = password;
 
 }
