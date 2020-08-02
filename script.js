ce1d201be0d1a7451df72bce9beb42bbfe30d1a3
Eric D. Torres <etorresnotary@gmail.com>
2020-08-02 17:19:10 -0500

    adding more special characters

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //special character array
-let special = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"];
+let special = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?", "<", ">", ":", ";", "[", "]", "{", "}"];
 
 //creating global variable to contain password length
 let passwordLength = 8;
