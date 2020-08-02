43134fc9dfa2987ee48c4aea26b68a33c224426e
Eric D. Torres <etorresnotary@gmail.com>
2020-08-01 20:59:40 -0500

    prompts function populating choice array with character arrays uppper/lower/number/special

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //special character array
-let special =["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"];
+let special = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"];
+
+//creating global variable to contain password length
+let passwordLength = 8;
 
 //array to hold choices from prompts
 let choice = [];
@@ -40,25 +43,41 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  let passwordLength = prompt("Please choose a length for you password \nPasswords must be a minimum of 8 characters and a maximum of 128 characters.");
-    if (!enter) {
-      alert("Please enter a value")
-    } else if (passwordLength < 8 || passwordLength > 128) {
-      alert("Please input a valid password length\nminimum number of characters = 8\nmaximum number of characters = 128")
-    } else {
-  let hasLower = confirm("Do you want your password to contain lower case characters?\n OK = YES and Cancel = NO");
-  let hasUpper = confirm("Do you want your password to contain upper case characters?\n OK = YES and Cancel = NO");
-  let hasNumber = confirm("Do you want your password to contain numbers\n OK = YES and Cancel = NO");
-  let hasSpecial = confirm("Do you want your password to contain special characters\n OK = YES and Cancel =NO")
-    }
-    if (!hasLower && !hasUpper && !hasNumber && !hasSpecial) {
-      choices = alert("Please choose at least one criteria")
-    }
-  
-function generatePassword();
-
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+
+}
+
+function prompts() {
+  choice = [];
+  passwordLength = prompt("Please choose a length for you password \nPasswords must be a minimum of 8 characters and a maximum of 128 characters.");
+  if (isNaN(passwordLength)) {
+    alert("Please enter a numerical value.");
+    return false;
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please input a valid password length\nminimum number of characters = 8\nmaximum number of characters = 128")
+  } else {
+    let hasLower = confirm("Do you want your password to contain lower case characters?\n OK = YES and Cancel = NO");
+    if (hasLower) {
+      choice = choice.concat(lower);
+    }
+    let hasUpper = confirm("Do you want your password to contain upper case characters?\n OK = YES and Cancel = NO");
+    if (hasUpper) {
+      choice = choice.concat(upper);
+    }
+    let hasNumber = confirm("Do you want your password to contain numbers\n OK = YES and Cancel = NO");
+    if (hasNumber) {
+      choice = choice.concat(number);
+    }
+    let hasSpecial = confirm("Do you want your password to contain special characters\n OK = YES and Cancel =NO")
+    if (hasSpecial) {
+      choice = choice.concat(special);
+    }
+    return true;
+  }
 }
 
 // Add event listener to generate button
