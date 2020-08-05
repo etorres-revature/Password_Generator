d75b585ea8304a17378beeda1733dd7731510893
Eric D. Torres <etorresnotary@gmail.com>
2020-08-05 15:48:42 -0500

    adding array to string and string to array

--- other builds/Password Generator(Strings to Arrays)/script.js
+++ other builds/Password Generator(Strings to Arrays)/script.js
@@ -1,9 +1,14 @@
-let strLower = "abcdefghijklmnopqrstuvwxyz".split("");
-let strUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-let strNumber = "0123456789".split("")
-let strSpecial = "!@#$%^&*()~][{}?>/".split("");
+let strLower = "abcdefghijklmnopqrstuvwxyz";
+// let strUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let strNumber = "0123456789";
+let strSpecial = "!@#$%^&*()~][{}?>/";
 let arrayChoices = [];
 let passwordLength = 8;
+
+// strLower.split("");
+// strSpecial.split("");
+// strNumber.split("");
+// strUpper.split('');
 
 
 // Assignment Code
@@ -25,7 +30,7 @@ function writePassword() {
 function generatePassword() {
   let newPassword;
   for (let i = 0; i < passwordLength; i++) {
-    newPassword = newPassword + arrayChoices(Math.floor(Math.random() * arrayChoices.length));
+    newPassword = newPassword + arrayChoices[Math.floor(Math.random() * arrayChoices.length)];
   }
   return newPassword;
 }
@@ -33,25 +38,38 @@ function generatePassword() {
 function userPrompts() {
   arrayChoices = [];
   passwordLength = prompt("Please choose a number between 8 and 128")
-  let hasLower = confirm("Would you like for your password to contain lower case letters?");
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("please enter an appropriate response")
+  } else {
+    let hasLower = confirm("Would you like for your password to contain lower case letters?");
     if (hasLower) {
-      arrayChoices.concat(strLower);
+      arrayChoices = arrayChoices.concat(strLower.split(""));
       console.log(arrayChoices);
     }
-let hasUpper = confirm("Would you like for your password to contain upper case letters?");
- if (hasUpper){
-   arrayChoices.concat(strUpper);
-   console.log(arrayChoices);
- }
- let hasNumber = confirm("Would you like for your password to contain numbers?");
-   if (hasNumber) {
-arrayChoices.concat(StrLower);
-console.log(arrayChoices);
-   }
-   let hasSpecial = confirm("Would you like for your password to contain special characters?");
-   if (hasSpecial) {
-     arrayChoices.concat(strSpecial);
-     console.log(arrayChoices)
-   }
- }
+    let hasUpper = confirm("Would you like for your password to contain upper case letters?");
+    if (hasUpper) {
+      arrayChoices = arrayChoices.concat(strLower.toUpperCase().split(""));
+      console.log(arrayChoices);
+    }
+    let hasNumber = confirm("Would you like for your password to contain numbers?");
+    if (hasNumber) {
+      arrayChoices = arrayChoices.concat(strNumber.split(""));
+      console.log(arrayChoices);
+    }
+    let hasSpecial = confirm("Would you like for your password to contain special characters?");
+    if (hasSpecial) {
+      arrayChoices = arrayChoices.concat(strSpecial.split(""));
+      console.log(arrayChoices)
+    } else if (!hasLower && !hasUpper && !hasNumber && !hasSpecial) {
+      alert("you have not chosen any characters with which to make your password")
+    }
+  }
+}
 
+
+// stringPractice = "435200afadfak;n;fwken";
+// stringPractice2 = "asdfpoaifnffpafoen";
+
+ //function splitString() {
+// console.log(stringPractice.split("").concat(stringPractice2.split("")));
+//  }
