1fbbcbdf455b6fb7e2f423b60125b208902562fb
Eric D. Torres <etorresnotary@gmail.com>
2020-08-04 19:38:44 -0500

    updating styling on other builds and updating logic for additional options left out originally

--- other builds/Password Generator(one function)/script.js
+++ other builds/Password Generator(one function)/script.js
@@ -15,7 +15,7 @@ function writePassword() {
   pLength = prompt("Please choose a password length.\nPasswords must be at least 8 characters long.\nPasswords may not be longer than 128 characters.")
   if (isNaN(pLength)) {
     console.log("need a number") 
-     alert("Pleae choose a numerical value"); 
+     alert("Please choose a numerical value"); 
   } else if (pLength < 8 || pLength > 128) {
     console.log("need a good value");
     alert("You have chosen an insufficient number of charactrers to create a password");
@@ -25,7 +25,7 @@ function writePassword() {
     let hasUpper = confirm("Would you like for your password to contain upper case lettters?\nOK button for 'YES'\nCANCEL button for 'NO'");
     let hasNumber = confirm("Would you like for your password to contain numbers?\nOK button for 'YES'\nCANCEL button for 'NO'");
     let hasSpecial = confirm("Would you like for your password to contain special characters?\nOK button for 'YES'\nCANCEL button for 'NO'");
-    if(hasLower && !hasUpper && !hasNumebr && !hasSpecial) {
+    if(hasLower && !hasUpper && !hasNumber && !hasSpecial) {
       newCharString = charString.slice(0,26);
       console.log(newCharString);
     } else if (hasLower && hasUpper && !hasNumber && !hasSpecial){
@@ -34,9 +34,15 @@ function writePassword() {
     } else if (hasLower && hasUpper && hasNumber && !hasSpecial) {
       newCharString = charString.substr(0, 62);
       console.log(newCharString);
+    } else if (!hasLower && hasUpper && !hasNumber && hasSpecial) {
+      newCharString = charString.slice(26, 52).concat(charString.slice(62, 80));
+      console.log(newCharString);
     } else if (hasLower && hasUpper && hasNumber && hasSpecial) {
       newCharString = charString;
       console.log(newCharString);
+     } else if (hasLower && !hasUpper && hasNumber && !hasSpecial){
+      newCharString = charString.substr(0, 26).concat(charString.substr(52,10));
+      console.log(newCharString)
      } else if (!hasLower && hasUpper && !hasNumber && !hasSpecial) {
         newCharString = charString.slice(26, 52);
         console.log(newCharString);
